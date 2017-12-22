c5b4312f83f5b2d88adcc29011574ad1fe2f0cd5
ekourtzanidis <s.kourtzanidis@outlook.com>
2017-12-22 12:52:32 +0200

    Revert "Fix #49"
    This reverts commit 38ea7127d80bad0981217c37ed544df90196ebfd.

--- Koans/AboutStrings.cs
+++ Koans/AboutStrings.cs
@@ -146,8 +146,16 @@ broken line";
             Assert.Equal(FILL_ME_IN, str);
         }
 
-
         [Step(14)]
+        public void VariablesCanBeInsertedInAStringInACoolerWay()
+        {
+            var firstName = "Matt";
+            var lastName = "Groves";
+            var str = $"My name is {firstName} {lastName}";
+            Assert.Equal(FILL_ME_IN, str);
+        }
+
+        [Step(15)]
         public void StringsCanBePaddedToTheLeft()
         {
             //You can modify the value inserted into the result
@@ -155,49 +163,49 @@ broken line";
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(15)]
+        [Step(16)]
         public void StringsCanBePaddedToTheRight()
         {
             var str = string.Format("{0,-3:}", "x");
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(16)]
+        [Step(17)]
         public void SeperatorsCanBeAdded()
         {
             var str = string.Format("{0:n}", 123456);
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(17)]
+        [Step(18)]
         public void CurrencyDesignatorsCanBeAdded()
         {
             var str = string.Format("{0:c}", 123456);
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(18)]
+        [Step(19)]
         public void NumberOfDisplayedDecimalsCanBeControled()
         {
             var str = string.Format("{0:.##}", 12.3456);
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(19)]
+        [Step(20)]
         public void MinimumNumberOfDisplayedDecimalsCanBeControled()
         {
             var str = string.Format("{0:.00}", 12.3);
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(20)]
+        [Step(21)]
         public void BuiltInDateFormaters()
         {
             var str = string.Format("{0:t}", DateTime.Parse("12/16/2011 2:35:02 PM", CultureInfo.InvariantCulture));
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(21)]
+        [Step(22)]
         public void CustomDateFormaters()
         {
             var str = string.Format("{0:t m}", DateTime.Parse("12/16/2011 2:35:02 PM", CultureInfo.InvariantCulture));
@@ -205,7 +213,7 @@ broken line";
         }
         //These are just a few of the formatters available. Dig some and you may find what you need.
 
-        [Step(22)]
+        [Step(23)]
         public void ABetterWayToConcatenateLotsOfStrings()
         {
             //Concatenating lots of strings is a Bad Idea(tm). If you need to do that, then consider StringBuilder.
@@ -225,7 +233,7 @@ broken line";
             //String.Format and StringBuilder will be more efficent that concatenation. Prefer them.
         }
 
-        [Step(23)]
+        [Step(24)]
         public void StringBuilderCanUseFormatAsWell()
         {
             var strBuilder = new System.Text.StringBuilder();
@@ -236,28 +244,28 @@ broken line";
             Assert.Equal(FILL_ME_IN, str);
         }
 
-        [Step(24)]
+        [Step(25)]
         public void LiteralStringsInterpretsEscapeCharacters()
         {
             var str = "\n";
             Assert.Equal(FILL_ME_IN, str.Length);
         }
 
-        [Step(25)]
+        [Step(26)]
         public void VerbatimStringsDoNotInterpretEscapeCharacters()
         {
             var str = @"\n";
             Assert.Equal(FILL_ME_IN, str.Length);
         }
 
-        [Step(26)]
+        [Step(27)]
         public void VerbatimStringsStillDoNotInterpretEscapeCharacters()
         {
             var str = @"\\\";
             Assert.Equal(FILL_ME_IN, str.Length);
         }
 
-        [Step(27)]
+        [Step(28)]
         public void YouCanGetASubstringFromAString()
         {
             var str = "Bacon, lettuce and tomato";
@@ -265,14 +273,14 @@ broken line";
             Assert.Equal(FILL_ME_IN, str.Substring(7, 3));
         }
 
-        [Step(28)]
+        [Step(29)]
         public void YouCanGetASingleCharacterFromAString()
         {
             var str = "Bacon, lettuce and tomato";
             Assert.Equal(FILL_ME_IN, str[0]);
         }
 
-        [Step(29)]
+        [Step(30)]
         public void SingleCharactersAreRepresentedByIntegers()
         {
             Assert.Equal(97, 'a');
@@ -280,7 +288,7 @@ broken line";
             Assert.Equal(FILL_ME_IN, 'b' == ('a' + 1));
         }
 
-        [Step(30)]
+        [Step(31)]
         public void StringsCanBeSplit()
         {
             var str = "Sausage Egg Cheese";
@@ -288,7 +296,7 @@ broken line";
             Assert.Equal(new[] { FILL_ME_IN }, words);
         }
 
-        [Step(31)]
+        [Step(32)]
         public void StringsCanBeSplitUsingCharacters()
         {
             var str = "the:rain:in:spain";
@@ -296,7 +304,7 @@ broken line";
             Assert.Equal(new[] { FILL_ME_IN }, words);
         }
 
-        [Step(32)]
+        [Step(33)]
         public void StringsCanBeSplitUsingRegularExpressions()
         {
             var str = "the:rain:in:spain";
@@ -309,7 +317,7 @@ broken line";
             //is highly recommended to be on your bookshelf
         }
 
-        [Step(33)]
+        [Step(34)]
         public void YouCanInterpolateVariablesIntoAString()
         {
             var name = "John Doe";
