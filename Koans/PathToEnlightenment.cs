f7ea9f4e9a54286fa979a6de9683112ee4ba6503
Bobby Johnson <bobby.johnson@gmail.com>
2020-09-25 14:27:25 -0700

    formatting path

--- Koans/PathToEnlightenment.cs
+++ Koans/PathToEnlightenment.cs
@@ -3,28 +3,28 @@ using DotNetCoreKoans.Engine;
 
 namespace DotNetCoreKoans.Koans
 {
-    public class PathToEnlightenment : Path
+  public class PathToEnlightenment : Path
+  {
+    public PathToEnlightenment()
     {
-        public PathToEnlightenment()
-        {
-            Types = new Type[] {
-		typeof(AboutAsserts),
-		typeof(AboutNull),
-		typeof(AboutArrays),
-		typeof(AboutArrayAssignment),
-		typeof(AboutStrings),
-                typeof(AboutFloats),
-	    	typeof(AboutDecimals),
-		typeof(AboutInheritance),
-                typeof(AboutMethods),
-                typeof(AboutControlStatements),
-                typeof(AboutGenericContainers),
-                typeof(AboutDelegates),
-		typeof(AboutLambdas),
-                typeof(AboutLinq),
-                typeof(AboutBitwiseAndShiftOperator),
-                typeof(AboutGlobalization)
+      Types = new Type[] {
+        typeof(AboutAsserts),
+        typeof(AboutNull),
+        typeof(AboutArrays),
+        typeof(AboutArrayAssignment),
+        typeof(AboutStrings),
+        typeof(AboutFloats),
+        typeof(AboutDecimals),
+        typeof(AboutInheritance),
+        typeof(AboutMethods),
+        typeof(AboutControlStatements),
+        typeof(AboutGenericContainers),
+        typeof(AboutDelegates),
+        typeof(AboutLambdas),
+        typeof(AboutLinq),
+        typeof(AboutBitwiseAndShiftOperator),
+        typeof(AboutGlobalization)
                 };
-        } 
     }
+  }
 }
