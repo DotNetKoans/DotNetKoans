c6c13bf0520b9cac14d6c00c0c33d102909d8406
Joshua Belden <github@joshuabelden.com>
2020-10-02 07:30:05 -0700

    Added Koans for the Enum type (#112)
    * Added Koans for the Enum type
    
    * Changes from pull request recomendations
    
    * Replacing Planet with something more zen like
    
    * Koan exercise updated to allow student to fill in the appropriate value

--- Koans/PathToEnlightenment.cs
+++ Koans/PathToEnlightenment.cs
@@ -16,6 +16,7 @@ namespace DotNetCoreKoans.Koans
         typeof(AboutStrings),
         typeof(AboutFloats),
         typeof(AboutDecimals),
+        typeof(AboutEnumerations),
         typeof(AboutInheritance),
         typeof(AboutMethods),
         typeof(AboutControlStatements),
