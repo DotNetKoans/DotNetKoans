c6e2e5089e76ae8d6f99feab27046bce7e1a3dfa
ibrahim-islam <ibrahim.islam@live.com>
2020-10-13 02:03:37 +0700

    adds disposable koan

--- Koans/PathToEnlightenment.cs
+++ Koans/PathToEnlightenment.cs
@@ -28,8 +28,8 @@ namespace DotNetCoreKoans.Koans
         typeof(AboutLambdas),
         typeof(AboutLinq),
         typeof(AboutBitwiseAndShiftOperator),
-        typeof(AboutGlobalization)
-
+        typeof(AboutGlobalization),
+        typeof(AboutDisposable)
         };
         }
     }
