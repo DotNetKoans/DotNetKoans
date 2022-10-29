a273d31eb93e52681aae677a9ba1db6798889209
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2022-10-29 21:04:33 +0200

    Resolve review comments (#168)

--- Koans/AboutDirectory.cs
+++ Koans/AboutDirectory.cs
@@ -1,45 +1,41 @@
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Collections;
-using System.Collections.Generic;
-using Xunit;
-using DotNetCoreKoans.Engine;
 using System.IO;
-using System.Text;
+using DotNetCoreKoans.Engine;
+using Xunit;
 using IOPath = System.IO.Path;
 
 namespace DotNetCoreKoans.Koans
 {
     public class AboutDirectory : Koan
     {
-        //Directory is a c# class that provides static methods for creating, moving, and enumerating through directories and subdirectories.
+        //Directory is a class that provides static methods for creating, moving, and enumerating through directories and subdirectories.
 
         private static string directoryName = "temp directory";       
-        private static string fullPath = IOPath.Combine(IOPath.GetTempPath(), directoryName);// GetTempPath() Returns the path of the current user's temporary folder.
+        private static string fullPath = IOPath.Combine(IOPath.GetTempPath(), directoryName); // GetTempPath() Returns the path of the current user's temporary folder.
 
         [Step(1)]
         public void CreatingAndDeletingDirectory()
         {            
             Directory.CreateDirectory(fullPath);
 
-            Assert.Equal(FILL_ME_IN, Directory.Exists(fullPath)); // How to make this statement true? 
+            Assert.Equal(FILL_ME_IN, Directory.Exists(fullPath));
 
             Directory.Delete(fullPath);
 
-            Assert.Equal(FILL_ME_IN, Directory.Exists(fullPath)); // How to make this statement true? 
+            Assert.Equal(FILL_ME_IN, Directory.Exists(fullPath));
         }
+        
         [Step(2)]
         public void GetDirectoryInfo()
         {
             var directoryInfo = new DirectoryInfo(fullPath);
             directoryInfo.Create();
 
-            Assert.Equal(FILL_ME_IN, directoryInfo.Exists); // How to make this statement true? 
-            Assert.Equal(FILL_ME_IN, directoryInfo.Name); // what is the directory name?
+            Assert.Equal(FILL_ME_IN, directoryInfo.Exists);
+            Assert.Equal(FILL_ME_IN, directoryInfo.Name);
 
             directoryInfo.Delete(false);
         }
+        
         [Step(3)]
         public void CreateSubDirectory()
         {
@@ -51,17 +47,18 @@ namespace DotNetCoreKoans.Koans
             Assert.Equal(FILL_ME_IN, directoryInfo.GetDirectories().Length); // what is the number of subdirectories?
 
             directoryInfo.Delete(true);
-
         }
+        
         [Step(4)]
         public void GetFilesInDirectory()
         {
-
-
             var directoryInfo = new DirectoryInfo(fullPath);
             directoryInfo.Create();
-            using (File.Create(IOPath.Combine(fullPath, "file1")));
-            using(File.Create(IOPath.Combine(fullPath, "file2")));
+            
+            using (File.Create(IOPath.Combine(fullPath, "file1")))
+            using (File.Create(IOPath.Combine(fullPath, "file2")))
+            {
+            }
 
             Assert.Equal(FILL_ME_IN, directoryInfo.GetFiles().Length); // what is the number of files that exist in this directory?
 
--- Koans/AboutFile.cs
+++ Koans/AboutFile.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Collections;
-using System.Collections.Generic;
 using Xunit;
 using DotNetCoreKoans.Engine;
 using System.IO;
@@ -13,19 +8,21 @@ namespace DotNetCoreKoans.Koans
 {
     public class AboutFile : Koan
     {
-        //File is async c# class that provides static methods for creating, copying, deleting, moving, and opening files, and helps create a FileStream object.
+        // File is a class that provides static methods for creating, copying, deleting, moving,
+        // and opening files, and helps in the creation of FileStream objects.
 
         [Step(1)]
         public void CreatingAndDeletingFile()
         {
             string path = IOPath.GetTempFileName(); // GetTempFileName() Creates a uniquely named, zero-byte temporary file on disk and returns the full path of that file.
             
-            Assert.Equal(FILL_ME_IN, File.Exists(path)); // How to make this statement true? 
+            Assert.Equal(FILL_ME_IN, File.Exists(path)); 
 
             File.Delete(path);
 
-            Assert.Equal(FILL_ME_IN, File.Exists(path)); // How to make this statement true? 
+            Assert.Equal(FILL_ME_IN, File.Exists(path));
         }
+        
         [Step(2)]
         public void CopyFile()
         {
@@ -35,25 +32,39 @@ namespace DotNetCoreKoans.Koans
             File.Delete(newPath);
             File.Copy(path, newPath);
 
-            Assert.Equal(FILL_ME_IN, File.Exists(newPath)); // How to make this statement true? 
-            
+            Assert.Equal(FILL_ME_IN, File.Exists(path));
+            Assert.Equal(FILL_ME_IN, File.Exists(newPath));
         }
+
         [Step(3)]
+        public void MoveFile()
+        {
+	        string path = IOPath.GetTempFileName();
+	        string newPath = IOPath.Combine(IOPath.GetTempPath(), "newFile.txt");
+	        
+	        File.Delete(newPath);
+	        File.Move(path, newPath);
+	        
+	        Assert.Equal(FILL_ME_IN, File.Exists(path));
+	        Assert.Equal(FILL_ME_IN, File.Exists(newPath));
+        }
+        
+        [Step(4)]
         public void GetFileInfo()
         {
             string path = IOPath.GetTempFileName();
             FileInfo fileInfo = new FileInfo(path);
 
-            Assert.Equal(FILL_ME_IN, fileInfo.Exists); // How to make this statement true? 
+            Assert.Equal(FILL_ME_IN, fileInfo.Exists);
             Assert.Equal(FILL_ME_IN, fileInfo.FullName); // what is the file name?
-
         }
-        [Step(4)]
+        
+        [Step(5)]
         public void ReadFile()
         {
 
             string data = "Hello World!";
-            string path = createFileAndFillIN(data);
+            string path = createFileAndFillIn(data);
 
             byte[] bytes = new byte[data.Length];
             UTF8Encoding temp = new UTF8Encoding(true);
@@ -68,12 +79,12 @@ namespace DotNetCoreKoans.Koans
             Assert.Equal(FILL_ME_IN, readMessage); // what is the message?
         }
 
-        [Step(5)]
+        [Step(6)]
         public void ReadLines()
         {
 
             string data = "Line0\nLine1\nLine2";
-            string path = createFileAndFillIN(data);
+            string path = createFileAndFillIn(data);
 
             var lines = File.ReadAllLines(path);
             
@@ -81,7 +92,7 @@ namespace DotNetCoreKoans.Koans
             Assert.Equal(FILL_ME_IN, lines[1]); // what is written in the line No.2 ?
         }
 
-        private string createFileAndFillIN(string data)
+        private string createFileAndFillIn(string data)
         {
             string path = IOPath.GetTempFileName();
             byte[] info = new UTF8Encoding(true).GetBytes(data);
@@ -91,7 +102,5 @@ namespace DotNetCoreKoans.Koans
             }
             return path;
         }
-
-
     }
 }
