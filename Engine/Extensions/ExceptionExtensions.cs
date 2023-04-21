40b1e532633b48cf5ed5c1595af2558152138d6d
keithythefrog <Keithythefrog@gmail.com>
2023-04-21 11:32:41 -0400

    - renamed namespace for Extension classes as suggested by Rider - renamed exceptionPathRegex to KoanExceptionPathRegex better clarify context - Updated Regex object to pattern match based on the Koan naming scheme starting with “About” -Output should now only show the relevant Koans Excercise

--- Engine/Extensions/ExceptionExtensions.cs
+++ Engine/Extensions/ExceptionExtensions.cs
@@ -1,15 +1,16 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace System
+namespace DotNetKoans.Engine.Extensions
 {
     public static class ExceptionExtensions
     {
-        private static readonly Regex exceptionPathRegex = new Regex(@"(\w*\.cs:line \d*)");
+        private static readonly Regex KoanExceptionPathRegex = new(@"(About\w*\.cs:line \d*)");
 
         public static IEnumerable<string> GetStackTracePaths(this Exception exception) =>
 
-            exceptionPathRegex.Matches(exception.StackTrace).Select(x => x.Value);
+            KoanExceptionPathRegex.Matches(exception.StackTrace).Select(x => x.Value);
     }
 }
--- Engine/Step.cs
+++ Engine/Step.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using DotNetKoans.Engine.Extensions;
 using Microsoft.DotNet.Cli.Utils;
 using Xunit.Sdk;
 
