874594736273310f705fa14c273dd99bd40df560
keithythefrog <Keithythefrog@gmail.com>
2023-04-21 11:54:09 -0400

    - Added Seperate “GetStackTracePath” like method that only provides an output based on a  pattern match for the “Asserts” (which appears to be Xunits naming scheme) it has to include the About negative lookbehind to prevent (notably AboutAssert.cs) a Koan from returning in the match - Updated console to output in format @kilasuit suggested.

--- Engine/Extensions/ExceptionExtensions.cs
+++ Engine/Extensions/ExceptionExtensions.cs
@@ -8,9 +8,13 @@ namespace DotNetKoans.Engine.Extensions
     public static class ExceptionExtensions
     {
         private static readonly Regex KoanExceptionPathRegex = new(@"(About\w*\.cs:line \d*)");
-
+        private static readonly Regex AssertExceptionPathRegex = new(@"\w*Asserts\.cs:line \d*(?<!About\w*\.cs:line \d*)");
+        
         public static IEnumerable<string> GetStackTracePaths(this Exception exception) =>
 
             KoanExceptionPathRegex.Matches(exception.StackTrace).Select(x => x.Value);
+        
+        public static IEnumerable<string> GetStackTracePathsAsserts(this Exception exception) =>
+            AssertExceptionPathRegex.Matches(exception.StackTrace).Select(x => x.Value);
     }
 }
